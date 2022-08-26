f13dc5989ee77a8d2ec570611413b400a53797ce
theodm <theo.dm94@gmail.com>
2022-08-26 05:39:10 +0000

    Tailwind-Css initialisiert

--- chameleon-client/src/index.tsx
+++ chameleon-client/src/index.tsx
@@ -9,6 +9,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     <ChameleonClient />
   </React.StrictMode>
 );
